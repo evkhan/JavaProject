fedce78707e800172be5ee675953db252b902764
bcannoe <36521191+bcannoe@users.noreply.github.com>
2018-02-18 22:48:48 -0500

    Add files via upload

---
+++ InClassProjectREVISED2/src/Address.java
@@ -0,0 +1,68 @@
+
+import javax.swing.JOptionPane;
+public class Address 
+{
+	String street;
+	String city;
+	String state;
+	int postalCode;
+	
+	public Address()
+	{
+		this.street = " ";
+		this.city = " ";
+		this.state = " ";
+		this.postalCode = 0;
+	}
+	public Address(String stre, String cit, String sta, int postCo) 
+	{
+		street = stre;
+		city = cit;
+		state = sta;
+		postalCode = postCo;
+	}
+	public void setStreet(String stre)
+	{
+		stre = JOptionPane.showInputDialog("Enter street name");
+		street = stre;
+	}
+	public String getStreet()
+	{
+		return street;
+	}
+	public void setCity(String cit)
+	{
+		cit = JOptionPane.showInputDialog("Enter your City");
+		city = cit;
+	}
+	public String getCity()
+	{
+		return city;
+	}
+	public void setState(String sta)
+	{
+		sta = JOptionPane.showInputDialog("Enter your State");
+		state = sta;
+	}
+	public String getState()
+	{
+		return state;
+	}
+	public void setPostalCode(int postCo)
+	{
+		postalCode = Integer.parseInt(JOptionPane.showInputDialog("Enter zipcode")); // string to integer conversion
+	}
+	public int getPostalCode()
+	{
+		return postalCode;
+	}
+	public String toString()
+	{
+		String result;
+		result = street + "\n" + city + ", " + state + " "  + postalCode;
+		return result;
+	}
+	
+
+	
+}
---
+++ InClassProjectREVISED2/src/Name.java
@@ -0,0 +1,56 @@
+
+import javax.swing.JOptionPane;
+public class Name 
+{
+	String first;
+	char initial;
+	String last;
+	
+		
+	public Name()
+	{
+		this.first = " ";
+		this.initial = ' ';
+		this.last = " ";
+	}
+	
+	public Name(String fir, char init, String las)
+	{
+		this.first = fir;
+		this.initial = init;
+		this.last = las;
+	}
+	public void setFirst(String fir)
+	{
+		fir = JOptionPane.showInputDialog("Enter first name");
+		first = fir;
+	}
+	public String getFirst()	
+	{
+		return first;
+	}
+	public void setInitial(char init)	// parameter still necessary for when method is called later
+	{
+		String input = JOptionPane.showInputDialog("Enter middle initial"); // input middle initial as a string
+		initial = input.charAt(0); //convert string to char (by reading the location in the string indicated) 
+	}
+	public char getInitial()
+	{
+		return initial;
+	}
+	public void setLast(String las)
+	{
+		las = JOptionPane.showInputDialog("Enter last name");
+		last = las;
+	}
+	public String getlast()
+	{
+		return last;
+	}
+	public String toString()
+	{
+		String result;
+		result = first + " " + initial + ". " + last;
+		return result;
+	}
+}
